145e26c089141630909327f5ab30adb60f3148af
simtf <fabiensimonet@gmail.com>
2018-08-08 22:12:53 +0200

    Jasmine unit tests

--- src/app/weather/weather.service.spec.ts
+++ src/app/weather/weather.service.spec.ts
@@ -1,9 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { inject, TestBed } from '@angular/core/testing'
 import { WeatherService } from './weather.service'
 
 describe('WeatherService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [WeatherService],
     })
   })
